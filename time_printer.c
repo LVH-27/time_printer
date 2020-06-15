4b0648e64562594ea246545a17d01767ee978125
LVH-27 <reo.the.yokel@gmail.com>
2020-06-15 23:09:27 +0200

    Changed output slightly

--- time_printer.c
+++ time_printer.c
@@ -10,7 +10,7 @@ int main() {
 
     now = time(NULL);
     ts = localtime(&now);
-    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ts);
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %Z", ts);
     printf("The time is %s\n", buf);
 
     return 0;
